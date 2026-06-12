815eaa38e93a691236367e962ff40ab590c2a7e3
Ahmed Mostafa <ahmedmostafa197754@gmail.com>
2026-06-12 17:49:39 +0300

    Delete src directory

--- src/api/rewardsApi.js
+++
@@ -1,8 +0,0 @@
-import { API_ENDPOINTS } from '../config/apiConfig.js';
-import { apiRequest } from './client.js';
-
-export const getDailyRewards = () => apiRequest(API_ENDPOINTS.rewards.daily);
-
-export const claimDailyReward = (rewardId) => apiRequest(API_ENDPOINTS.rewards.claimDaily(rewardId), {
-  method: 'POST',
-});
