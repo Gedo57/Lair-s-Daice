0a7360dff3237d7382530208c5f4cee2a9635ca9
Ahmed Mostafa <ahmedmostafa197754@gmail.com>
2026-06-13 14:55:21 +0300

    Delete src directory

--- src/api/eventsApi.js
+++
@@ -1,9 +0,0 @@
-import { API_ENDPOINTS } from '../config/apiConfig.js';
-import { apiRequest } from './client.js';
-
-export const getSpecialEvents = () => apiRequest(API_ENDPOINTS.events.list);
-export const getEventMissions = () => apiRequest(API_ENDPOINTS.events.missions);
-
-export const playSpecialEvent = (eventId) => apiRequest(API_ENDPOINTS.events.play(eventId), {
-  method: 'POST',
-});
--- src/api/profileApi.js
+++
@@ -1,7 +0,0 @@
-import { API_ENDPOINTS } from '../config/apiConfig.js';
-import { apiRequest } from './client.js';
-
-export const getProfile = () => apiRequest(API_ENDPOINTS.profile.me);
-export const getWallet = () => apiRequest(API_ENDPOINTS.profile.wallet);
-
-export const updateProfile = (payload) => apiRequest(API_ENDPOINTS.profile.update, { method: 'PATCH', body: payload });
