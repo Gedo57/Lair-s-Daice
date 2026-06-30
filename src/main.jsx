df58c2f1d390e6aba8cda80a1df3522ca5ed9036
Gedo57 <ahmedmostafa197754@gmail.com>
2026-06-30 22:23:49 +0300

    2321312

--- src/main.jsx
+++ src/main.jsx
@@ -60,6 +60,7 @@ import './styles/mobile/portrait-tournament-pass-fix.mobile.css';
 import './styles/mobile/mobile-landscape-browser-fix.css';
 import './styles/base/gameplay-chat.css';
 import './styles/base/bots-mode.css';
+import './styles/mobile/portrait-gameplay-final-reference.mobile.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
