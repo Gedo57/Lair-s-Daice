188de6973829696f419040b1760792b42dfb1516
Gedo57 <ahmedmostafa197754@gmail.com>
2026-07-09 13:36:06 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -73,6 +73,7 @@ import './styles/base/gameplay-zai-timer-patch.css';
 import './styles/mobile/portrait-gameplay-manual-controls.mobile.css';
 import './styles/mobile/portrait-gameplay-background-blur.mobile.css';
 import './styles/base/current-bid-time-reminder-style.css';
+import './styles/base/bid-selector-transparency-controls.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
